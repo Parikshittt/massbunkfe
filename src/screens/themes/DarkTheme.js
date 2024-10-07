9a043cca0f14fdd7828328122dc953a62ecf74e3
parik <parikshitsingh1104@gmail.com>
2024-10-07 14:10:44 +0530

    Added dark theme file

---
+++ src/screens/themes/DarkTheme.js
@@ -0,0 +1,13 @@
+export const DarkTheme = {
+    color: {
+        primary: '#121212',
+        secondary: '#1E2A38',
+        tertiary: '#FDF8EE',
+    },
+    fonts: {
+        regular: 'Poppins-Regular',
+        medium: 'Poppins-Medium',
+        bold: 'Poppins-Bold',
+        vintage: 'vintage-waves.regular'
+    }
+}
