48306a3eafce96d54336994de4924b9b48c11279
parik <parikshitsingh1104@gmail.com>
2024-10-07 13:55:14 +0530

    Changed tsk to jsx + Added custom fonts

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+function Main() {
+  return (
+    <>
+      <SafeAreaView>
+        <Text style={{ fontFamily: 'Poppins-Bold', fontSize: 20 }}>Hello World</Text>
+      </SafeAreaView>
+    </>
+  )
+}
+
+export default Main;
